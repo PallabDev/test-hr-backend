ac9176420ac7901a4fa7c0f88a2f5ee9a20bb403
Pallab Karmakar <ookarmakarpallab1212@gmail.com>
2025-09-08 19:10:27 +0530

    adding fix;

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser";
 import { client } from "./whatsappClient.js";
+
 const app = express();
 app.use(cors({
     origin: process.env.ORIGIN,
